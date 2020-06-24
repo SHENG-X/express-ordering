94fc3bf8f60a8bc107155cb311524c6e2422eba8
Sheng Xiao <sheng26v@gmail.com>
2020-06-24 14:20:36 -0700

    chore: add page header and style router buttons

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/auth">Auth</router-link>
-    </div>
+    <a-page-header
+      class="header"
+      title="Express Ordering"
+      sub-title="xxx Restaurant"
+    >
+      <template slot="extra">
+        <router-link to="/">Home</router-link>
+        <router-link to="/auth">Auth</router-link>
+      </template>
+    </a-page-header>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#nav {
-  display: flex;
-
+<style lang="scss" scoped>
+.header{
+  border: 1px solid rgb(235, 237, 240);
+}
+.ant-page-header-heading-extra {
   a {
     font-weight: bold;
     color: #2c3e50;
+    font-size: 18px;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #1890ff;
     }
   }
 }
