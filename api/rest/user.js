bfb682cbcd8b4733d3cb68e001005383cf48c1e2
Sheng Xiao <sheng26v@gmail.com>
2020-06-24 01:47:38 -0700

    chore: use query instead of params
    - get method uses query instead of using params

--- api/rest/user.js
+++ api/rest/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/:email', (req, res) => {
+router.get('/', (req, res) => {
   res.status(200).send("get a user");
 });
 
