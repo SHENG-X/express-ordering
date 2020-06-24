da16a2afb3df36acaffa40fa213b93579225ecba
Sheng Xiao <sheng26v@gmail.com>
2020-06-24 01:46:36 -0700

    chore: used ant design for all ui components
    - added ant design vue components to the project
    - globally included all components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue';
+import Antd from 'ant-design-vue';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import 'ant-design-vue/dist/antd.css';
+
 Vue.config.productionTip = false;
+
+// import all ant design components for now
+// TODO: investigate if all compnoents are needed
+Vue.use(Antd);
 
 new Vue({
   router,
