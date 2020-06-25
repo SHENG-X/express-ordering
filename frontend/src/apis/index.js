b0e4a32589bb7de356062d82d63b0f1f5aa751d8
Sheng Xiao <sheng26v@gmail.com>
2020-06-24 19:05:45 -0700

    feat: font-end functions for api access

---
+++ frontend/src/apis/index.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const endPoint = process.env.API_END_POINT || 'http://localhost:8080';
+
+export const signIn = async ({ email, password }) => {
+  try {
+    const response = await axios.post(`${endPoint}/auth`, {
+      email,
+      password,
+    });
+    if (response.status === 200) {
+      console.log('Sign In Success!!!');
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const signUp = async ({
+  fname,
+  lname,
+  phone,
+  email,
+  password,
+}) => {
+  try {
+    const response = await axios.post(`${endPoint}/user`, {
+      fname,
+      lname,
+      phone,
+      email,
+      password,
+    });
+    if (response.status === 201) {
+      console.log('Sign Up Success!!!');
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
