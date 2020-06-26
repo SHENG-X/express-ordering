77a3a228b8c7ca1380ff9ae23326d508c07b7ad5
Sheng Xiao <sheng26v@gmail.com>
2020-06-25 17:13:21 -0700

    feat: api access point for authenticate a user

--- api/rest/auth.js
+++ api/rest/auth.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../../database/model/user');
+
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  res.status(200).send("authenticate a user");
+router.post('/', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    const valid = await bcrypt.compareSync(password, user.password);
+    if (valid) {
+      return res.status(200).json({ ...user._doc, password: null });
+    }
+  }
+  return res.status(401).json('unauthorized');
 });
 
 module.exports = router;
