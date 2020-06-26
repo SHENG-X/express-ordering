1394d52d59256d9c8b621c6e365a73477dec758b
Sheng Xiao <sheng26v@gmail.com>
2020-06-25 17:15:39 -0700

    chore: use findOne to check if a user exists

--- api/rest/user.js
+++ api/rest/user.js
@@ -2,31 +2,34 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 
-const { logger } = require('../../util/helper');
 const User = require('../../database/model/user');
 
 const saltRounds = 10;
 
 router.get('/', async (req, res) => {
   const email = req.query.email;
-  const user = await User.find({email: email});
-  if(user.length){
+  const user = await User.findOne({ email });
+  if (user) {
     return res.status(409).json('email was registered in the system');
   }
   res.status(200).json('email was not registered in the system');
 });
 
 router.post('/', async (req, res) => {
-  const encryptedPassword = await bcrypt.hashSync(req.body.password, saltRounds);
-  const user = new User({ ...req.body, password: encryptedPassword });
-  console.log(encryptedPassword);
-  await user.save((err, doc) => {
-    if (err) {
-      logger.error(err);
-      res.status(500).json(err);
-    }
-    res.status(201).json({...doc._doc, password: null});
-  });
+  const email = req.body.email;
+  const user = await User.findOne({ email });
+  if (user) {
+    return res.status(409).json('email was registered in the system');
+  } else {
+    const encryptedPassword = await bcrypt.hashSync(req.body.password, saltRounds);
+    const user = new User({ ...req.body, password: encryptedPassword });
+    await user.save((err, doc) => {
+      if (err) {
+        res.status(500).json(err);
+      }
+      res.status(201).json({...doc._doc, password: null});
+    });
+  }
 });
 
 router.put('/', (req, res) => {
