16a09b6318a3754d69130fdb1a5d6e23c2475e66
Sheng Xiao <sheng26v@gmail.com>
2020-06-30 19:03:12 -0700

    chore: moved user related logics to user service module
    - use validator middleware validate email

--- api/rest/user.js
+++ api/rest/user.js
@@ -1,40 +1,15 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const { body } = require('express-validator');
+
+const userService = require('../../services/userService');
 
 const router = express.Router();
 
-const User = require('../../database/model/user');
-const { translateUser } = require('../../util/helper'); 
+router.get('/', [], userService.exist);
 
-const saltRounds = 10;
+router.post('/', [body('email').isEmail()], userService.signUp);
 
-router.get('/', async (req, res) => {
-  const email = req.query.email;
-  const user = await User.findOne({ email });
-  if (user) {
-    return res.status(409).json('email was registered in the system');
-  }
-  res.status(200).json('email was not registered in the system');
-});
-
-router.post('/', async (req, res) => {
-  const email = req.body.email;
-  const user = await User.findOne({ email });
-  if (user) {
-    return res.status(409).json('email was registered in the system');
-  } else {
-    const encryptedPassword = await bcrypt.hashSync(req.body.password, saltRounds);
-    const user = new User({ ...req.body, password: encryptedPassword });
-    await user.save((err, doc) => {
-      if (err) {
-        res.status(500).json(err);
-      }
-      const token = jwt.sign(translateUser(user), process.env.JWT_SECRET, { expiresIn: '6h' });
-      res.status(201).json(token);
-    });
-  }
-});
+router.post('/sign-in', [body('email').isEmail()], userService.signIn);
 
 router.put('/', (req, res) => {
   res.status(200).send("update a user");
