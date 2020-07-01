1d2f84ce2bdc81e0db0dbf26d77e233a3e64e6d7
Sheng Xiao <sheng26v@gmail.com>
2020-06-30 18:57:22 -0700

    chore: authentication was moved to user route

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const bodyParser = require('body-parser');
 
 const { logger } = require('./util/helper');
 const user = require('./api/rest/user');
-const auth = require('./api/rest/auth');
 
 const port = process.env.NODE_ENV === 'development-host' ? 
              process.env.HOST_PORT : process.env.PORT;
@@ -20,7 +19,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use('/user', user);
-app.use('/auth', auth);
 
 const options = {
   useNewUrlParser: true,
