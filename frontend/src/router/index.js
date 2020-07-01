d764ae65f286059db717314220390046efa14849
Sheng Xiao <sheng26v@gmail.com>
2020-06-30 19:10:46 -0700

    chore: only user with admin privilege can access dashboard

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import store from '../store';
 
 Vue.use(VueRouter);
 
@@ -26,6 +27,23 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.name === 'dashboard') {
+    // require admin privilege to access dashboard
+    let admin = false;
+    if (store.state.user) {
+      admin = store.state.user.admin;
+    }
+    if (admin) {
+      next();
+    } else {
+      next({ name: 'home' });
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
