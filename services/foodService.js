e35117e8dd59421eb3f39fb80f73fde1557bb538
Sheng Xiao <sheng26v@gmail.com>
2020-07-09 16:42:11 -0700

    feat: food services for managing food

--- services/foodService.js
+++ services/foodService.js
@@ -2,12 +2,29 @@ const foodModel = require('../database/model/foodModel');
 
 const getFood = (req, res) => {
   // get a food detail by id
+  if (Object.entries(req.query).length) {
+    return foodModel.findById(req.query._id, (error, food) => {
+      if (error) {
+        res.status(500).json(error);
+      } else {
+        res.status(200).json(food);
+      }
+    });
+  } else {
+    return foodModel.find({}, (error, food) => {
+      if (error) {
+        res.status(500).json(error);
+      } else {
+        res.status(200).json(food);
+      }
+    });
+  }
 }
 
-const createFood = async (req, res) => {
+const createFood = (req, res) => {
   // create a new food
   const food = new foodModel({...req.body});
-  await food.save((error, doc) => {
+  return food.save((error, doc) => {
     if (error) {
       res.status(500).json(error);
     } else {
