a89904b9a2857f73843c48872fdb4a6942aef605
Sheng Xiao <sheng26v@gmail.com>
2020-07-09 16:44:05 -0700

    feat: initial implementation of the create food modal component

---
+++ frontend/src/components/CreateFoodModal.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="create-food-modal">
+    <a-form-model
+      :label-col="labelCol"
+      :wrapper-col="wrapperCol"
+      layout="horizontal"
+    >
+
+      <a-form-model-item
+        has-feedback
+        :label="'Name'"
+        prop="name"
+      >
+        <a-input
+          type="text"
+          v-model="food.name"
+          placeholder="name"
+        />
+      </a-form-model-item>
+
+      <a-form-model-item
+        has-feedback
+        :label="'Description'"
+        prop="description"
+      >
+        <a-textarea
+          placeholder="Food description here."
+          v-model="food.description"
+          :rows="4"
+        />
+      </a-form-model-item>
+
+      <a-form-model-item
+        has-feedback
+        :label="'Price'"
+        prop="description"
+      >
+        <a-input-number
+          :default-value="0.00"
+          :formatter="value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
+          :parser="value => value.replace(/\$\s?|(,*)/g, '')"
+          v-model="food.price"
+        />
+      </a-form-model-item>
+
+      <a-form-model-item
+        has-feedback
+        :label="'Picture'"
+        prop="description"
+        class="uploader-row"
+      >
+        <div class="dropbox">
+          <a-upload
+            name="picture"
+            action="http://localhost:8081/picture"
+            list-type="picture-card"
+            @preview="handlePreview"
+            @change="handleChange"
+          >
+            <div v-if="fileList.length < 1">
+              <a-icon type="plus" />
+              <div class="ant-upload-text">
+                Upload
+              </div>
+            </div>
+          </a-upload>
+          <a-modal :visible="previewVisible" :footer="null" @cancel="handleCancel">
+            <img alt="example" style="width: 100%" :src="previewImage" />
+          </a-modal>
+        </div>
+      </a-form-model-item>
+
+      <a-form-model-item :wrapper-col="{ span: 14, offset: 4 }">
+        <a-button type="primary" @click="onSubmit">
+          Create
+        </a-button>
+      </a-form-model-item>
+
+    </a-form-model>
+  </div>
+</template>
+
+<script>
+import getBase64 from '@/util';
+import { createFood } from '@/services/foodService';
+
+export default {
+  data() {
+    return {
+      labelCol: { span: 4 },
+      wrapperCol: { span: 14 },
+      previewVisible: false,
+      previewImage: '',
+      fileList: [],
+      food: {
+        image: '',
+        name: '',
+        description: '',
+        price: '',
+      },
+    };
+  },
+  methods: {
+    async onSubmit() {
+      console.log('onSubmit', this.food);
+      debugger;
+      const response = await createFood(this.food);
+      console.log('response', response);
+    },
+    handleCancel() {
+      this.previewVisible = false;
+    },
+    async handlePreview(file) {
+      if (!file.url && !file.preview) {
+        file.preview = await getBase64(file.originFileObj);
+      }
+      this.previewImage = file.url || file.preview;
+      this.previewVisible = true;
+    },
+    handleChange({ fileList }) {
+      this.fileList = fileList;
+      if (fileList[0].status === 'done') {
+        this.food.image = fileList[0].response;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.create-food-modal {
+  .ant-input-number {
+    width: 50%;
+    min-width: 132px;
+    max-width: 260px;
+  }
+  .uploader-row {
+    height: 127px;
+    margin-bottom: 0;
+  }
+}
+</style>
