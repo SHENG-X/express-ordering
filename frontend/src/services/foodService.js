f1651fcd56b20d7723b8ceeadded5c2789e847f7
Sheng Xiao <sheng26v@gmail.com>
2020-07-09 23:31:32 -0700

    chore: get food service to get food

--- frontend/src/services/foodService.js
+++ frontend/src/services/foodService.js
@@ -1,5 +1,12 @@
 import service from './index';
 
+export const getFood = (fid) => {
+  if (fid) {
+    return service.get('/food', { _id: fid });
+  }
+  return service.get('/food');
+};
+
 export const createFood = (food) => service.post('/food', { ...food });
 
 export const updateFood = (food) => service.put('/food', { ...food });
