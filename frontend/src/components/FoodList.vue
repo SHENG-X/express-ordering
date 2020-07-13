a730fc797c97c8ed35fcdb46d929420658308e26
Sheng Xiao <sheng26v@gmail.com>
2020-07-13 16:37:42 -0700

    chore: style and add functionalities to food list component

--- frontend/src/components/FoodList.vue
+++ frontend/src/components/FoodList.vue
@@ -2,19 +2,31 @@
   <div class="container">
     <a-list bordered :data-source="food">
       <a-list-item slot="renderItem" slot-scope="item">
-        <div>
-          <img :src="`http://localhost:8081/assets/${item.image}`" width="60px"/>
+        <div class="image-container">
+          <img
+            :src="`${imageBaseURL}/assets/${item.image}`"
+            width="60px" height="60px"
+          />
         </div>
-        <div>{{ item.name }}</div>
-        <div>{{ item.description }}</div>
-        <div>$ {{ item.price }}</div>
+        <div class="name">
+          {{ item.name }}
+        </div>
         <div>
-          <a-button type="primary">
+          {{ item.description }}
+        </div>
+        <div class="price">
+          $ {{ item.price }}
+        </div>
+        <div class="button-container">
+          <a-button
+            type="primary"
+            @click="() => setCurrentFood(item)"
+          >
             Edit
           </a-button>
           <a-button
             type="danger"
-            @click="() => deleteSingleFood(item._id)"
+            @click="() => deleteSingleFood(item)"
           >
             Delete
           </a-button>
@@ -28,23 +40,33 @@
 import { getFood, deleteFood } from '@/services/foodService';
 
 export default {
+  props: {
+    setCurrentFood: {
+      type: Function,
+      required: true,
+    },
+  },
   data() {
     return {
       food: [],
     };
+  },
+  computed: {
+    imageBaseURL() {
+      return process.env.VUE_APP_API_END_POINT;
+    },
   },
   async created() {
     const food = await getFood();
     this.food = food.data;
   },
   methods: {
-    async deleteSingleFood(id) {
+    async deleteSingleFood(food) {
       try {
-        await deleteFood(id);
+        await deleteFood(food);
       } catch (error) {
-        console.log(error.response);
         if (error.response.status === 404) {
-          this.food = this.food.filter((food) => food._id !== error.response.data);
+          this.food = this.food.filter((f) => f._id !== error.response.data);
         }
       }
     },
@@ -57,6 +79,30 @@ export default {
   height: 100%;
   .ant-list {
     height: 100%;
+    overflow: auto;
+    .ant-list-item {
+      display: flex;
+      div {
+        flex: 1;
+        min-width: 120px;
+      }
+      .name {
+        padding: 0 24px;
+        width: 180px;
+      }
+      .price {
+        max-width: 120px;
+        padding: 0 24px;
+      }
+      .image-container {
+        max-width: 80px;
+      }
+      .button-container {
+        width: 160px;
+        display: flex;
+        justify-content: flex-end;
+      }
+    }
   }
 }
 </style>
