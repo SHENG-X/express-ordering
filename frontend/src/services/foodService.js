2720f5e13c842b0965f6e639ae512182594da936
Sheng Xiao <sheng26v@gmail.com>
2020-07-13 16:38:48 -0700

    chore: delete food service send id and image file name to server side

--- frontend/src/services/foodService.js
+++ frontend/src/services/foodService.js
@@ -11,4 +11,4 @@ export const createFood = (food) => service.post('/food', { ...food });
 
 export const updateFood = (food) => service.put('/food', { ...food });
 
-export const deleteFood = (id) => service.delete('/food', { data: { _id: id } });
+export const deleteFood = (food) => service.delete('/food', { data: { _id: food._id, image: food.image } });
