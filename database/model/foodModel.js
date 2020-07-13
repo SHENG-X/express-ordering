f999fbc90584ddde6fb94dfbf86761a2375f9b1a
Sheng Xiao <sheng26v@gmail.com>
2020-07-13 16:57:27 -0700

    chore: food model use category as a foreign key

--- database/model/foodModel.js
+++ database/model/foodModel.js
@@ -18,6 +18,10 @@ const foodSchema = new mongoose.Schema(
       type: Number,
       default: 0
     },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category'
+    },
   },
 );
 
