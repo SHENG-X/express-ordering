539384bd101113871f03e527c2e40e75f3d168a7
Sheng Xiao <sheng26v@gmail.com>
2020-07-15 14:27:00 -0700

    chore: show food cards on home page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,13 +1,46 @@
 <template>
   <div class="home">
-    Coming Soon
+    <div class="food-container">
+      <food-card
+        v-for="food in getFood"
+        :food="food"
+        :key="food._id"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex';
+
+import FoodCard from '@/components/FoodCard.vue';
+
 export default {
   name: 'Home',
   components: {
+    FoodCard,
+  },
+  computed: {
+    ...mapGetters([
+      'getFood',
+    ]),
+  },
+  created() {
+    this.loadFWithC();
+  },
+  methods: {
+    ...mapActions([
+      'loadFWithC',
+    ]),
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.food-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(460px, 1fr));
+  align-items: center;
+  justify-items: center;
+}
+</style>
