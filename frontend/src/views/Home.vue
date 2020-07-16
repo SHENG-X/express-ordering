f7c85764e996f5c4035826d93232c298890f8bd3
Sheng Xiao <sheng26v@gmail.com>
2020-07-15 19:19:59 -0700

    chore: single food ordering modal

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -5,8 +5,48 @@
         v-for="food in getFood"
         :food="food"
         :key="food._id"
+        @click.native="() => showModal(food)"
       />
     </div>
+    <a-modal
+      v-model="visible"
+      @ok="handleOk"
+      v-if="currentFood"
+    >
+      <template slot="footer">
+        <a-button key="increase" @click="() => handleFoodCount(1)">
+          Increase
+        </a-button>
+         <a-input-number
+          :min="1"
+          :max="1000"
+          :default-value="1"
+          v-model="foodCount"
+        />
+        <a-button key="decrease" @click="() => handleFoodCount(-1)">
+          Decrease
+        </a-button>
+        <a-button key="addCart" type="primary" @click="handleOk">
+          <div>Add to cart - ${{ (currentFood.price * foodCount).toFixed(2) }}</div>
+        </a-button>
+      </template>
+      <div class="order">
+        <div class="name">
+          {{ currentFood.name}}
+        </div>
+        <div class="description">
+          {{ currentFood.description }}
+        </div>
+        <div class="preferences">
+          Preferences
+        </div>
+        <div class="extra">
+          <div>Extra instructions</div>
+          <div>List any special requests</div>
+        </div>
+        <a-textarea placeholder="e.g. allergies, extra spicy, etc." :rows="4" />
+      </div>
+    </a-modal>
   </div>
 </template>
 
@@ -20,6 +60,13 @@ export default {
   components: {
     FoodCard,
   },
+  data() {
+    return {
+      visible: false,
+      currentFood: null,
+      foodCount: 1,
+    };
+  },
   computed: {
     ...mapGetters([
       'getFood',
@@ -32,6 +79,20 @@ export default {
     ...mapActions([
       'loadFWithC',
     ]),
+    showModal(food) {
+      if (this.currentFood && food._id !== this.currentFood._id) {
+        // reset food count on openning a new food item
+        this.foodCount = 1;
+      }
+      this.currentFood = food;
+      this.visible = true;
+    },
+    handleFoodCount(base) {
+      if (this.foodCount === 1 && base < 1) return;
+      this.foodCount += base * 1;
+    },
+    handleOk() {
+    },
   },
 };
 </script>
@@ -42,5 +103,21 @@ export default {
   grid-template-columns: repeat(auto-fit, minmax(460px, 1fr));
   align-items: center;
   justify-items: center;
+}
+.order {
+  .name {
+    font-size: 18px;
+    font-weight: 600;
+  }
+  .preferences {
+    font-size: 16px;
+    font-weight: 600;
+    padding: 8px 0;
+  }
+  .extra {
+    font-weight: 600;
+    display: flex;
+    justify-content: space-between;
+  }
 }
 </style>
